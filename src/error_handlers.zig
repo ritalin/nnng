edce7e7fa9699c6983dd24d3c869fc68a914d5a3
Ritalin <kazu.ktz.tamura@gmail.com>
2026-05-05 16:07:54 +0900

    Update error code

--- src/error_handlers.zig
+++ src/error_handlers.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const root = @import("./root.zig");
 const c = @import("c");
 
@@ -8,6 +9,7 @@ pub fn open_error(err: c_int) root.OpenError {
     if (err == c.NNG_ENOTSUP) {
         return error.NotSupported;
     }
+    std.log.err("open_error/unhandled code: {}", .{err});
     unreachable;
 }
 
@@ -15,6 +17,7 @@ pub fn close_error(err: c_int) root.CloseError {
     if (err == c.NNG_ECLOSED) {
         return error.AlreadyClosed;
     }
+    std.log.err("close_error/unhandled code: {}", .{err});
     unreachable;
 }
 
@@ -28,6 +31,7 @@ pub fn new_transport_error(err: c_int) root.NewTransportError {
     if (err == c.NNG_ENOMEM) {
         return error.OutOfMemory;
     }
+    std.log.err("new_transport_error/unhandled code: {}", .{err});
     unreachable;
 }
 
@@ -62,6 +66,10 @@ pub fn start_transport_error(err: c_int) root.StartTransportError {
     if (err == c.NNG_EUNREACHABLE) {
         return error.Unreachable;
     }
+    if (err == c.NNG_EADDRINUSE) {
+        return error.AddressInUse;
+    }
+    std.log.err("start_transport_error/unhandled code: {}", .{err});
     unreachable;
 }
 
@@ -72,12 +80,13 @@ pub fn open_aio_pipe_error(err: c_int) root.OpenAioPipeError {
     if (err == c.NNG_ENOTSUP) {
         return error.NotSupported;
     }
+    std.log.err("open_aio_pipe_error/unhandled code: {}", .{err});
     unreachable;
 }
 
 pub fn send_error(err: c_int) root.SendError {
     if (err == c.NNG_EAGAIN) {
-        return error.Blocked;
+        return error.WouldBlock;
     }
     if (err == c.NNG_ECLOSED) {
         return error.AlreadyClosed;
@@ -95,17 +104,22 @@ pub fn send_error(err: c_int) root.SendError {
         return error.NotSupported;
     }
     if (err == c.NNG_ESTATE) {
-        return error.Unreachable;
+        return error.InvalidState;
     }
     if (err == c.NNG_ETIMEDOUT) {
         return error.Timeout;
     }
+    if (err == c.NNG_ECANCELED) {
+        return error.Canceled;
+    }
+
+    std.log.err("send_error/unhandled code: {}", .{err});
     unreachable;
 }
 
 pub fn receive_error(err: c_int) root.ReceiveError {
     if (err == c.NNG_EAGAIN) {
-        return error.Blocked;
+        return error.WouldBlock;
     }
     if (err == c.NNG_ECLOSED) {
         return error.AlreadyClosed;
@@ -120,10 +134,15 @@ pub fn receive_error(err: c_int) root.ReceiveError {
         return error.NotSupported;
     }
     if (err == c.NNG_ESTATE) {
-        return error.Unreachable;
+        return error.InvalidState;
     }
     if (err == c.NNG_ETIMEDOUT) {
         return error.Timeout;
     }
+    if (err == c.NNG_ECANCELED) {
+        return error.Canceled;
+    }
+
+    std.log.err("receive_error/unhandled code: {}", .{err});
     unreachable;
 }
