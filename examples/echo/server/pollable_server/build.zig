21446a18ef109bcf53f9062d6ab52531fe496e61
Ritalin <kazu.ktz.tamura@gmail.com>
2026-05-06 13:12:29 +0900

    Add pollable echo server

---
+++ examples/echo/server/pollable_server/build.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const pkg_prefix = b.option([]const u8, "NNG_PREFIX", "Native dependency root path") orelse "/use/local";
+
+    const dep_nnng = b.dependency("nnng", .{
+        .NNG_PREFIX = pkg_prefix,
+        .target = target,
+        .optimize = optimize,
+    });
+    const dep_supports = b.dependency("echo_support", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "echo-server-poolable",
+        .root_module = b.addModule("server", .{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "nnng", .module = dep_nnng.artifact("nnng").root_module },
+                .{ .name = "echo_support", .module = dep_supports.module("echo_support") },
+            },
+        }),
+    });
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| run_cmd.addArgs(args);
+
+    const run_cmd_step = b.step("run", "Launch echo server");
+    run_cmd_step.dependOn(&run_cmd.step);
+}
