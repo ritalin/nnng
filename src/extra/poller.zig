50f23efba867cd8547a76e808ecec41b664686a5
Ritalin <kazu.ktz.tamura@gmail.com>
2026-05-25 23:46:32 +0900

    Apply poll options

--- src/extra/poller.zig
+++ src/extra/poller.zig
@@ -59,7 +59,7 @@ pub fn ReceivePoller(comptime buffer_size: comptime_int) type {
         ///
         /// Behavior is undefined if re-entered.
         ///
-        pub fn poll(self: *Poller, callback: WakeupCallback) !usize {
+        pub fn poll(self: *Poller, callback: WakeupCallback, options: Options) !usize {
             var ready_iter = self.ready_set.keyIterator();
 
             const cpus = try std.Thread.getCpuCount();
@@ -70,9 +70,10 @@ pub fn ReceivePoller(comptime buffer_size: comptime_int) type {
 
                 if (self.poller_pipes.get(id.*)) |pipe| {
                     const channel = try self.context.allocator.create(ReadyChannel);
+                    const force_concurrent = options.force_concurrent orelse (self.in_fight_set.count() > cpus - 1);
 
                     try self.in_fight_set.put(id.*, {});
-                    try self.tasks.attach(id.*, pipe, channel, self.in_fight_set.count() > cpus - 1);
+                    try self.tasks.attach(id.*, pipe, channel, force_concurrent);
                     i += 1;
                 }
             }
@@ -267,6 +268,10 @@ pub const PollEvent = union(enum) {
         id: u64,
         err: ReceiveError,
     },
+};
+
+pub const Options = struct {
+    force_concurrent: ?bool = null,
 };
 
 pub const Timeout = union {
@@ -514,7 +519,7 @@ pub const tests = struct {
         };
 
         try TestPoller.Sync.attach(&poller, &rep_socket.pipe);
-        _ = try poller.poll(PollCallback.replyMsg);
+        _ = try poller.poll(PollCallback.replyMsg, .{});
 
         receive_msg: {
             var msg = try req_pipe1.receiver().drain(.{ .flags = .{ .nonblocking = false }});
@@ -629,7 +634,7 @@ pub const tests = struct {
 
         var accept: usize = 0;
         while (accept < 2) {
-            accept += try poller.poll(PollCallback.replyMsg);
+            accept += try poller.poll(PollCallback.replyMsg, .{});
         }
 
         receive_msg: {
@@ -712,7 +717,7 @@ pub const tests = struct {
         try std.testing.expectEqual(cpus * 2, cb.poller.ready_set.count());
         try std.testing.expectEqual(0, cb.poller.in_fight_set.count());
 
-        const n = try cb.poller.poll(PollCallback.replyMsg);
+        const n = try cb.poller.poll(PollCallback.replyMsg, .{});
         try std.testing.expectEqual(1, n);
         try std.testing.expectEqual(true, cb.called);
 
@@ -802,7 +807,7 @@ pub const tests = struct {
 
         try TestPoller.Parallel.attach(&poller, &rep_socket.pipe);
 
-        const accept= try poller.poll(PollCallback.replyMsg);
+        const accept= try poller.poll(PollCallback.replyMsg, .{});
         try std.testing.expectEqual(1, accept);
 
         receive_msg: {
@@ -893,7 +898,7 @@ pub const tests = struct {
 
         var accept: usize = 0;
         while (accept < 3) {
-            accept += try poller.poll(PollCallback.replyMsg);
+            accept += try poller.poll(PollCallback.replyMsg, .{});
         }
         try std.testing.expectEqual(3, accept);
     }
